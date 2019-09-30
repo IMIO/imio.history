56d25c0581e49965ed6573a37f8e9308e3fe6373
Gauthier Bastien <g.bastien@imio.be>
2019-09-30 21:49:41 +0200

    Call allow_module 'utils' outside the iniialize method is it is not necessary to declare imio.history as additional z2 products in tests relying on it

--- src/imio/history/__init__.py
+++ src/imio/history/__init__.py
@@ -1,8 +1,11 @@
-# -*- extra stuff goes here -*-
+# -*- coding: utf-8 -*-
+
+from AccessControl import allow_module
+
+
+allow_module('imio.history.utils')
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
 
-    from AccessControl import allow_module
-    allow_module('imio.history.utils')
