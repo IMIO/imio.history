abe499c84aa691597910078a61ccc6b962fad217
Gauthier Bastien <gauthier.bastien@imio.be>
2023-10-27 13:13:20 +0200

    Back to development: 1.32

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = (
 
 setup(
     name='imio.history',
-    version='1.31',
+    version='1.32.dev0',
     description="Imio history",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
